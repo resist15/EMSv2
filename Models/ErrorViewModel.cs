64d52a6273c77a0ec069f0831f1d2e7e4b503018
resist15 <souravhjagtap21@gmail.com>
2025-07-08 00:55:14 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMSv2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
